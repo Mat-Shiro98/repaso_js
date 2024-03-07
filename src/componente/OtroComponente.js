efd2d014ad74b7f746cbd44bc390b4572c4b4ff7
TheZhyro98 <matias_shiroma@hotmail.com>
2024-03-07 19:16:53 -0300

    Repaso 07-03-24

--- src/componente/OtroComponente.js
+++ src/componente/OtroComponente.js
@@ -6,11 +6,32 @@ const otroComponente = () => {
         alert("has dado click al boton!! " + nombre);
     }
 
+    const hasDadoDobleClick = () => {
+        alert("Has dado Doble Click!!")
+    }
+
+    const hasEntrado = (e,action) =>{
+        console.log(`Has ${action} a la caja`);
+    }
+
     return (
         <div className='otroComponente'>
             <h1>Evento Componente</h1>
 
-            <button onClick={e => hasDadoClick(e,"Victor")}>Dame Click!</button>
+            <p>
+                <button onClick={e => hasDadoClick(e, "Victor")}>Dame Click!</button>
+            </p>
+
+            <p>
+                <button onDoubleClick={hasDadoDobleClick}>Dame Doble Click!</button>
+            </p>
+
+            <div id="caja"
+                onMouseEnter={ e => hasEntrado(e,"entrado")}
+                onMouseLeave={ e => hasEntrado(e,"salido")}
+            >
+                <p>Estoy dentro de la caja</p>
+            </div>
 
         </div>
     )
